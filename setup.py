b6286a720c79d2873397a5fc4dc10adb53f53d39
Khole Jones <khole.jones@studio.co.uk>
2020-09-30 21:24:23 +0100

    update Version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 
 setup(
     name='pyhiveapi',
-    version='0.2.19.5',
+    version='0.2.20.2',
     description='A Python library to interface with the Hive API',
     long_description="A Python library to interface with the Hive API",
     url='https://github.com/Rendili/pyhiveapi',
