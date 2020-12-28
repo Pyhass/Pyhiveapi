f6655f1d47edf200c2f3883dcc2c3ac225924eae
Khole Jones <khole.jones@studio.co.uk>
2020-12-28 13:33:58 +0000

    Version 3.1.1

--- pyhiveapi/__init__.py
+++ pyhiveapi/__init__.py
@@ -1,10 +1,11 @@
 """__init__.py"""
 from .hive import Hive
 from .hive_session import Session
-from .const import *
-from .helper import HiveHelper
-from .client import Client
 from .hive_api import HiveApi
 from .hive_async_api import HiveAsync
 from .hive_auth import HiveAuth
 from .hive_auth_async import HiveAuthAsync
+from .const import *
+from .helper import HiveHelper
+from .hive_exceptions import *
+from .client import Client
--- pyhiveapi/const.py
+++ pyhiveapi/const.py
@@ -1,3 +1,4 @@
 """Constants for Pyhiveapi."""
 PACKAGE_NAME = "Pyhiveapi"
 PACKAGE_DIR = "/pyhiveapi/"
+SMS_REQUIRED = "SMS_MFA"
