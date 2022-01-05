0d460118e55cbe733af7b58309c58a3c8686c771
Khole <ktech6@outlook.com>
2022-01-05 19:16:58 +0000

    Update version. to 0.4.6

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def requirements_from_file(filename="requirements.txt"):
 
 
 setup(
-    version="0.4.5",
+    version="0.4.6",
     package_data={"data": ["*.json"]},
     include_package_data=True,
     cmdclass={
