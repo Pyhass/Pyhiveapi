e6f7c0d55cce663d4b4a3fec8ce0b015b523bd2b
Khole Jones <kjtech6@outlook.com>
2022-10-03 12:35:22 +0100

    Fix Version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements_from_file(filename="requirements.txt"):
 
 
 setup(
-    version="0.5.13",
+    version="0.5.14",
     package_data={"data": ["*.json"]},
     include_package_data=True,
     cmdclass={
