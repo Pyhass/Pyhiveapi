e2372ae803cfa541a9089f4b74eed2c601d790a0
Khole Jones <KJonline@github.com>
2025-01-12 21:52:20 +0000

    V1.0.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements_from_file(filename="requirements.txt"):
 
 
 setup(
-    version="0.5.17",
+    version="1.0.0",
     package_data={"data": ["*.json"]},
     include_package_data=True,
     cmdclass={
