3b6839649a50aa9fa629ad9fb8d751494f2abefe
Khole <29937485+KJonline@users.noreply.github.com>
2025-07-25 10:49:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def requirements_from_file(filename="requirements.txt"):
 
 
 setup(
-    version="1.0.2",
+    version="1.0.3",
     package_data={"data": ["*.json"]},
     include_package_data=True,
     cmdclass={
