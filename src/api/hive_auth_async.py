6c08beda2bf426424c941c31aab1f3f5245cb700
Khole Jones <29937485+KJonline@users.noreply.github.com>
2026-05-17 18:35:29 +0100

    chore: use datetime.timezone.utc instead of datetime.UTC for Python 3.10 compatibility

--- src/api/hive_auth_async.py
+++ src/api/hive_auth_async.py
@@ -214,7 +214,9 @@ class HiveAuthAsync(DeviceRegistrationMixin):
         timestamp = re.sub(
             r" 0(\d) ",
             r" \1 ",
-            datetime.datetime.now(datetime.UTC).strftime("%a %b %d %H:%M:%S UTC %Y"),
+            datetime.datetime.now(datetime.timezone.utc).strftime(
+                "%a %b %d %H:%M:%S UTC %Y"
+            ),
         )
         hkdf = await self.loop.run_in_executor(
             None,
